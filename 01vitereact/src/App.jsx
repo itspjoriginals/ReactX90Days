960a503274f459067c96b974e46367b425ed021a
Prashant Kumar Jha <itspjprashant07@gmail.com>
2025-07-30 22:26:11 +0530

    Some new learnings

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,11 @@
+import Chai from "./chai"
 function App() {
 
   return (
-    <h1>Hello React this is Prashant</h1>
+<>
+<h1>Hello React this is Prashant</h1>
+<Chai />
+</>
   )
 }
 
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(0)
+
+
+  const addValue = ()=>{
+    if(count=='20'){
+      setCount(20);
+    }
+    else{
+      setCount(count+1)
+    }
+  }
+
+  const removeValue = () =>{
+    if(count==0){
+      setCount(0);
+    }
+    else{
+      setCount(count -1);
+    }
+  }
+
+  return (
+    <>
+    <h1>Chai aur React</h1>
+    <h2>Counter Value: {count}</h2>
+    <button onClick={addValue}>Add Value</button>
+    <br />
+    <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
