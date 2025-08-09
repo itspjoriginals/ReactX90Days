0a79d22ab8f34fca726a2bd2decb154b8e82acd7
Prashant Kumar Jha <itspjprashant07@gmail.com>
2025-08-09 21:44:59 +0530

    Add some new changes to reactRouter Project

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='bg-green-600 p-4'>React Router</h1>
+    {/* <h1 className='bg-green-600 p-4'>React Router</h1> */}
+    {/* <Header />
+    <Home />
+    <Footer /> */}
     </>
   )
 }
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,11 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+// Create routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // Optional: handle route-level errors
+    errorElement: <h1>Page not found or an error occurred</h1>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
