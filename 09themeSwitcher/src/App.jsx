c368f76744cda5371eea2bc29e0f183981f373e0
Prashant Kumar Jha <itspjprashant07@gmail.com>
2025-08-12 18:27:34 +0530

    Add Theme Changer project

---
+++ 09themeSwitcher/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { ThemeProvider } from './context/theme'
+
+function App() {
+return (
+<ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+  <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* Theme Button */}
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       {/* Card */}
+                    </div>
+                </div>
+            </div>
+</ThemeProvider>
+
+  )
+}
+
+export default App
